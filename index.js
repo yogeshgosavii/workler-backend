168c75e9bf3cf06e4753e743246b353f3409a115
yogesh <yogeshgosavif8@gmail.com>
2024-06-16 09:43:13 +0530

    prod commit 37

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import authRoutes from './routes/authRoutes'; // Ensure authRoutes is properly exported
-// import jobRoutes from './routes/jobRoutes';
-// import profileRoutes from './routes/profileRoutes';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 
@@ -22,26 +20,24 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-console.log("MONGO_URI:", MONGO_URI); // Log MONGO_URI to check if it's being read correctly
+console.log("MONGO_URI:", MONGO_URI);
 
 if (!MONGO_URI) {
     console.error('Error: MONGO_URI is not defined in environment variables');
     process.exit(1);
 }
 
-// Test MongoDB connection separately
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: 'worklerData',
 }).then(() => {
     console.log('Connected to MongoDB');
-    // Start the server only after a successful MongoDB connection
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 }).catch((err) => {
-    console.error('Failed to connect to MongoDB', err);
+    console.error('Failed to connect to MongoDB:', err.message);
     process.exit(1);
 });
 
@@ -50,11 +46,7 @@ app.get("/", (req, res) => {
     res.send("prod running with MongoDB connection");
 });
 
-// Enable authRoutes
 app.use('/api/auth', authRoutes);
-
-// app.use('/api/jobs', jobRoutes);
-// app.use('/api/profile', profileRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
