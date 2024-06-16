475d1d44304ac58631c9a42e36633cdd3439b0b5
yogesh <yogeshgosavif8@gmail.com>
2024-06-16 07:07:51 +0530

    prod7 commit

--- index.js
+++ index.js
@@ -53,6 +53,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const jobRoutes = require('./routes/jobRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 
 dotenv.config();
 
@@ -87,8 +90,12 @@ mongoose.connect(MONGO_URI, {
 });
 
 app.get("/", (req, res) => {
-    res.send("MongoDB connected");
+    res.send("Full setup running with routes");
 });
+
+app.use('/api/auth', authRoutes);
+app.use('/api/jobs', jobRoutes);
+app.use('/api/profile', profileRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
