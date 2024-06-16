ca75d5da0432fcd22a51e31dfc1dc4cfac24d3dd
yogesh <yogeshgosavif8@gmail.com>
2024-06-16 07:04:59 +0530

    prod6 commit

--- index.js
+++ index.js
@@ -50,15 +50,48 @@
 //     console.log(`Server is running on port ${PORT}`);
 // });
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Render!');
+if (!MONGO_URI) {
+    console.error('Error: MONGO_URI is not defined in environment variables');
+    process.exit(1);
+}
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'worklerData',
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
+});
+
+app.get("/", (req, res) => {
+    res.send("MongoDB connected");
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
