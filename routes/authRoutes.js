b1895eb94a18dce1fe1ce54977a489a6f85caf19
yogesh <yogeshgosavif8@gmail.com>
2024-06-17 09:52:50 +0530

    prod commit 57

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 const router = Router();
-import { signup, login, checkEmail, getUserDetails } from '../controllers/authController.js'; 
-import { protect } from '../middleware/authMiddleware.js';
+import { checkEmail,} from '../controllers/authController.js'; 
+// import { protect } from '../middleware/authMiddleware.js';
 
-router.post('/signup', signup);
-router.post('/login', login);
+// router.post('/signup', signup);
+// router.post('/login', login);
 router.post('/check-email', checkEmail);
 
 // Example of protecting a route with authentication middleware
