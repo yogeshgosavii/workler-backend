6d1bc2c12d5dbf2208a48011c5a2dff3193c6b90
yogesh <yogeshgosavif8@gmail.com>
2024-06-23 09:30:35 +0530

    prod commit 103

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
 };
