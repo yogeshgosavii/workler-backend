814de047e3002b9daa5840210585673f4d7bc203
yogesh <yogeshgosavif8@gmail.com>
2024-06-23 06:03:14 +0530

    prod commit 80

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,9 @@
 import { Router } from 'express';
 const router = Router();
-// import { signup, login, checkEmail , getUserDetails } from '../controllers/authController.js'; 
 
-
-// Comment out the import of authController.js
-// import auth from '../controllers/authController.js'; 
-
-// Define a simple route to ensure the router is working
+// Simple test route to verify the router is working
 router.get('/test', (req, res) => {
     res.send('Test route is working');
 });
-
-// Comment out other routes until the issue is resolved
-// router.post('/signup', signup);
-// router.post('/login', login);
-// router.post('/check-email', checkEmail);
-// router.get('/user', getUserDetails);
 
 export default router;
