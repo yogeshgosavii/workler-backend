3f580db5bca8dba4406ebb620c8977c46a1d9f78
yogesh <yogeshgosavif8@gmail.com>
2024-06-23 07:40:55 +0530

    prod commit 90

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 // routes/authRoutes.js
 
 import { Router } from 'express';
-import { testAuth } from '../controllers/authController.js'; 
+import { checkEmail,testAuth } from '../controllers/authController.js'; 
 
 const router = Router();
 
 // Simple test route to verify the controller is working
+router.post('/check-email', checkEmail);
 router.get('/testAuth', testAuth);
 
 export default router;
