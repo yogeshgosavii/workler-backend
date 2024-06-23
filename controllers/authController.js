cbb1a16195d4459933ebbd970e178333ac0be84c
yogesh <yogeshgosavif8@gmail.com>
2024-06-23 07:31:21 +0530

    prod commit 85

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,5 @@
 // Example of improved error handling in authController.js
-import bcrypt from 'bcrypt';
-import User from '../models/userModel.js';
 
-export const signup = async (req, res) => {
-    const { email, password, username, birthDate, accountType } = req.body;
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ email, password: hashedPassword, username, birthDate, accountType });
-        await user.save();
-        console.log(`User created: ${user.email}`);
-        res.status(201).send('User created successfully');
-    } catch (error) {
-        console.error('Error creating user:', error);
-        res.status(500).send('Error creating user');
-    }
-};
 
 export const testAuth = (req, res) => {
     res.send('Auth controller test is working');
