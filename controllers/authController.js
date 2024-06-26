f8f4bca7f96e4145aa3f759e06a0c6987405fb53
yogesh <yogeshgosavif8@gmail.com>
2024-06-26 13:19:25 +0530

    prod commit 115

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
 import { jwtSecret } from '../config.js';
 
