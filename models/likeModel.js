da8054128a69e028857c32855b30c75498e78387
Yogesh <yogeshgosavif8@gmail.com>
2024-08-08 00:36:56 +0530

    prod commit 194

--- models/likeModel.js
+++ models/likeModel.js
@@ -3,18 +3,21 @@ import { Schema } from "mongoose";
 const likeSchema = new Schema({
   post: {
     type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Post'
   },
   user: {
     type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
   },
-
   like_type: {
     type: String,
     enum: ['thumbs_up', 'heart', 'star'],
-    default : 'heart'
+    default: 'heart'
   },
 }, { timestamps: true });
 
-likeSchema.index({ post: 1, user: 1 }, { unique: true });
+likeSchema.index({ post: 1, user: 1 }, { unique: true, sparse: true });
 
 export default likeSchema;
